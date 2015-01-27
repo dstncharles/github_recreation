637c5d51c0ab3d87a55d9d67ab746b638d9e1842
Dustin Morando <dustinmorando@me.com>
2015-01-27 16:16:52 -0500

    add basic boilerplate and div structure

---
+++ main.js
@@ -0,0 +1,11 @@
+//1.Build HTML for skeleton of page
+//2.Determin what needs templated
+////repos
+////profiles
+////organzation
+//3.Fetch repo data form API using AJAX
+//4.forEach data console.log(title,date)
+//5.creat one template for a repository
+//6.forEach data for each respository log template string
+//7.Render on page
+// 
